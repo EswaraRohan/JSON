5385a2bf856e69d672c8f9a67c26575b39a3a4be
EswaraRohan <77735061+EswaraRohan@users.noreply.github.com>
2022-07-19 20:33:50 +0530

    Add files via upload

---
+++ JSON/c4.js
@@ -0,0 +1,14 @@
+var mycar={};
+mycar.car1 = "black";
+console.log(mycar);
+mycar.car2="red";
+console.log(mycar);
+
+var mycar = {};
+mycar["car1"] = "black";
+console.log(mycar);
+mycar["car2"] = "red";
+console.log(mycar);
+
+var mycar = {"car1":"black","car2":"red"};
+console.log(mycar);
